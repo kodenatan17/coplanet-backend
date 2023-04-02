78cc93e88cb026f8cf7b614d274e3f296b00861e
ferry-wibisono <ferry.wibisono@salt.co.id>
2023-04-02 13:16:46 +0700

    fixing on gitignore

--- config/index.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require('dotenv');
-
-dotenv.config()
-
-module.exports = {
-    serviceName: process.env.SERBICE_NAME,
-    urlDb: process.env.MONGO_URL
-}
--- db/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const { urlDb } = require('../config')
-
-mongoose.connect(urlDb, { useUnifiedTopology: true, useNewUrlParser: true });
-
-const db = mongoose.connection
-module.exports = db
