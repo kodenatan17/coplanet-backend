cd17e28c25fd0130f384e518af14b4949a7fe64b
ferry-wibisono <ferry.wibisono@salt.co.id>
2023-04-02 16:55:35 +0700

    adding service nominal

--- app/nominal/controller.js
+++ app/nominal/controller.js
@@ -24,64 +24,64 @@ module.exports = {
             res.redirect('/nominal')
         }
     },
-    // actionCreate: async (req, res) => {
-    //     try {
-    //         const { name } = req.body
-    //         let category = await Category({ name })
-    //         await category.save()
+    actionCreate: async (req, res) => {
+        try {
+            const { coinName, coinQuantity, price } = req.body
+            let nominal = await Nominal({ coinName, coinQuantity, price })
+            await nominal.save()
 
-    //         req.flash('alertMessage', 'Berhasil tambah kategori')
-    //         req.flash('alertStatus', 'success');
-    //         res.redirect('/category')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/category')
-    //     }
-    // },
-    // viewEdit: async (req, res) => {
-    //     try {
-    //         const { id } = req.params
-    //         const category = await Category.findOne({ _id: id });
-    //         res.render('admin/category/edit', { category })
+            req.flash('alertMessage', 'Berhasil tambah nominal')
+            req.flash('alertStatus', 'success');
+            res.redirect('/nominal')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/nominal')
+        }
+    },
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params
+            const nominal = await Nominal.findOne({ _id: id });
+            res.render('admin/nominal/edit', { nominal })
 
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/category')
-    //     }
-    // },
-    // actionEdit: async (req, res) => {
-    //     try {
-    //         const { id } = req.params
-    //         const { name } = req.body
-    //         await Category.findOneAndUpdate({
-    //             _id: id
-    //         }, { name });
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/nominal')
+        }
+    },
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params
+            const { coinName, coinQuantity, price } = req.body
+            await Nominal.findOneAndUpdate({
+                _id: id
+            }, { coinName, coinQuantity, price });
 
-    //         req.flash('alertMessage', 'Berhasil ubah kategori')
-    //         req.flash('alertStatus', 'success');
-    //         res.redirect('/category')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/category')
-    //     }
-    // },
-    // actionDelete: async (req, res) => {
-    //     try {
-    //         const { id } = req.params
-    //         await Category.findOneAndRemove({
-    //             _id: id
-    //         });
+            req.flash('alertMessage', 'Berhasil ubah nominal')
+            req.flash('alertStatus', 'success');
+            res.redirect('/nominal')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/nominal')
+        }
+    },
+    actionDelete: async (req, res) => {
+        try {
+            const { id } = req.params
+            await Nominal.findOneAndRemove({
+                _id: id
+            });
 
-    //         req.flash('alertMessage', 'Berhasil hapus kategori')
-    //         req.flash('alertStatus', 'success');
-    //         res.redirect('/category')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`)
-    //         req.flash('alertStatus', 'danger')
-    //         res.redirect('/category')
-    //     }
-    // }
+            req.flash('alertMessage', 'Berhasil hapus nominal')
+            req.flash('alertStatus', 'success');
+            res.redirect('/nominal')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`)
+            req.flash('alertStatus', 'danger')
+            res.redirect('/nominal')
+        }
+    }
 }
