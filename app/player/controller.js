869b371dd90533b5d9cfb34f11b90ef6d8c42b5f
ferry-wibisono <ferry.wibisono@salt.co.id>
2023-06-11 10:26:17 +0700

    api service detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -5,10 +5,23 @@ module.exports = {
     landingPage: async (req, res) => {
         try {
             const voucher = await Voucher.find().select('_id name status category thumbnail ').populate('category')
+            res.status(200).json({ data: voucher, success: true })
+
+        } catch (e) {
+            res.status(500).json({ message: e.message || 'Internal server error' })
+        }
+    },
+    detailPage: async (req, res) => {
+        try {
+            const { id } = req.params
+            const voucher = await Voucher.findOne().select({ _id: id }).populate('category').populate('nominals').populate('user', '_id name phoneNumber')
+            if (!voucher) {
+                return res.status(404).json({ message: "No voucher found" })
+            }
             res.status(200).json({ data: voucher })
 
         } catch (e) {
-            res.status(500).json({ message: e.message || 'Terjadi kesalahan pada server' })
+            res.status(500).json({ message: e.message || 'Internal server error' })
         }
     }
 }
--- app/player/router.js
+++ app/player/router.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const { landingPage } = require('./controller');
+const { landingPage, detailPage } = require('./controller');
 
 /* GET home page. */
 router.get('/landing-page', landingPage);
+router.get('/:id/detail', detailPage);
+
 
 module.exports = router;
