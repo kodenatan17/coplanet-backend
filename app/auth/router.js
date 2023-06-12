1766821ae69951b67c7096743234fe69179ea7d0
ferry-wibisono <ferry.wibisono@salt.co.id>
2023-06-12 19:10:26 +0700

    make api sign-in

--- app/auth/router.js
+++ app/auth/router.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { signUp } = require('./controller');
+const { signUp, signIn } = require('./controller');
 const multer = require('multer');
 const os = require('os');
 
 /* GET home page. */
 router.post('/sign-up', multer({ dest: os.tmpdir() }).single('image'), signUp);
-
+router.post('/sign-up', signIn);
 
 module.exports = router;
